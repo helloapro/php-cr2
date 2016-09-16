4037bc0eab244666ac7dab8af5dbcc28235c1d82
April Peng <helloaprilpeng@gmail.com>
2016-09-16 12:23:48 -0700

    add front end pages, styles, img, and functionality to project

--- app/app.php
+++ app/app.php
@@ -7,7 +7,22 @@
 
   $app = new Silex\Application();
 
+  $app['debug'] = true;
+
   $app->register (new Silex\Provider\TwigServiceProvider(), array('twig.path' => __DIR__.'/../views'));
+
+  $app->get('/', function() use ($app) {
+     return $app['twig']->render('word-frequency-form.html.twig');
+  });
+
+  $app->post('/process', function() use ($app) {
+     $inputSentence = $_POST['sentence'];
+     $inputWord = $_POST['word'];
+
+     $newRepeatCounter = new RepeatCounter;
+     $result = $newRepeatCounter->countRepeats($inputWord, $inputSentence);
+     return $app['twig']->render('word-frequency-results.html.twig', array('result'=>$result));
+  });
 
 
   return $app;
