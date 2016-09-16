bea2e5d5e004fb391f8f5c818b4a97e085619fd4
April Peng <helloaprilpeng@gmail.com>
2016-09-16 09:01:35 -0700

    set up test for first spec

--- tests/WordFrequencyTest.php
+++ tests/WordFrequencyTest.php
@@ -1,6 +1,18 @@
 <?php
-    class WordFrequencyTest extends PHPUnit_TestCase
+    require_once 'src/WordFrequency.php';
+
+    class WordFrequencyTest extends PHPUnit_Framework_TestCase
     {
-        
+        function test_word_search()
+        {
+            $test_repeatCounter = new RepeatCounter;
+            $inputWord = "bike";
+            $inputSentence = "I love riding my bike to brunch!";
+
+            $result = $test_repeatCounter->countRepeats($input);
+
+            $this->assertEquals(true, $result);
+        }
+
     }
 ?>
